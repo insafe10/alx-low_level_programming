cf2f7ec706eeff2b861338738c0b1bcd519dbd31
insafe10 <essaadyinsafe@gmail.com>
2023-02-23 14:24:09 -0800

    04- more f

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul-checks for checks for a digit (0 throgh 9)
+ * @a: a-Variable
+ * @b: b-Variable
+ * Return: Always0.
+ */
+int mul(int a, int b)
+{
+int mul;
+
+mul = a * b;
+
+return (mul);
+}
