0cd1dbc5c85e2f0059ca90a5b6f5152ebc5d85ff
insafe10 <essaadyinsafe@gmail.com>
2023-03-10 16:57:39 -0800

    argv argc

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-	#include <stdlib.h>
-	#include <ctype.h>
-	#include <string.h>
-	/**
-	 * check_num - check - string there are digit
-	 * @str: array str
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int check_num(char *str)
-	{
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
 		/*Declaring variables*/
 		unsigned int count;
 
@@ -31,13 +31,13 @@
 	}
 
 
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
 
 	int main(int argc, char *argv[])
@@ -80,4 +80,4 @@
 
 
 		return (0);
-	}
+}
