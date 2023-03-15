e3c805550b238c649803a12814011cd67171590d
insafe10 <essaadyinsafe@gmail.com>
2023-03-14 17:02:16 -0700

    alloc free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,7 +10,6 @@ char *argstostr(int ac, char **av)
 {
 	int i, n,  r = 0, l = 0;
 	char *str;
-	
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
@@ -37,5 +36,5 @@ char *argstostr(int ac, char **av)
 			str[r++] = '\n';
 		}
 	}
-	 return (str);
+	return (str);
 }
