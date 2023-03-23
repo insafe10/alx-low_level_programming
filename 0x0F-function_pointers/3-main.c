39539d2fdc281a70de3ed6b24037c17e774730ba
insafe10 <essaadyinsafe@gmail.com>
2023-03-22 17:59:45 -0700

    function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,7 @@
  *
  * Return: Always 0
  */
-int main(int __attribute__((__unused__))argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
@@ -24,7 +24,7 @@ int main(int __attribute__((__unused__))argc, char *argv[])
 	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	if ( get_op_func(op) == NULL || op[1] != '\0')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 			exit(99);
@@ -40,4 +40,4 @@ int main(int __attribute__((__unused__))argc, char *argv[])
 	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
-}			
+}
